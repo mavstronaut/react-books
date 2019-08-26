228d10683644d688a38e9f39f8a3d4778ae0eecb
mavstronaut <mavbarona@gmail.com>
2019-08-26 13:31:05 -0400

    initial commit

---
+++ create-react-express/client/build/precache-manifest.8fba2458a04657410cdab8f5c02a80fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e64cfa6002200a2181a3",
+    "url": "/static/css/main.8f2df384.chunk.css"
+  },
+  {
+    "revision": "e64cfa6002200a2181a3",
+    "url": "/static/js/main.390cf938.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cfea2ce5f4ad87b91a9b",
+    "url": "/static/js/2.9e90cea9.chunk.js"
+  },
+  {
+    "revision": "7fb013d757e6afdd921f4d99fcfb569f",
+    "url": "/index.html"
+  }
+];
